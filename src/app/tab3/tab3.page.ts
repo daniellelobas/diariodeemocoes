8984737490da18295e379bea15b3c0b25103ede5
daniellelobas <daniellelobas@gmail.com>
2024-01-03 08:11:13 -0300

    atualização

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
+import { 
+  AngularFireDatabase,
+  AngularFireList,
+  AngularFireObject } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +13,20 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
-  constructor() {}
-
+  titulo: string;
+  descricao: string;
+  dailys: Observable<any[]>;
+  constructor(private db: AngularFireDatabase) {
+    this.getDailys();
+  }
+  getDailys(){
+    this.dailys = this.db.list('dailys').valueChanges();
+  }
+  createdl(){
+    this.db.list('dailys').push({titulo: this.titulo, descricao: this.descricao})
+    .then(() => {
+      window.alert('Daily created successfully!');
+    })
+  }
 }
+
